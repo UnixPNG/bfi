f3530bf8aaebc9a9d98ee8c76ca372df21a9c23d
UnixPNG <maniarhythm@gmail.com>
2023-01-05 10:14:41 -0800

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,28 @@ fn remove_whitespace(s: &mut String) {
 }
 fn main() -> () {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
+    if args.len() < 2 {
         println!("Please provide a file to interpret.");
     }
     else{
     let mut data: Vec<i8> = [0; 30000].to_vec();
     let mut save_me: Vec<u8> = vec![0];
-    let mut argindex: usize = 0;
+    let mut argindex: usize = 2;
     let mut index: usize = 0;
     let mut skip: usize = 0;
-    let mut help_me = "".to_string();
     let file = &args[1];
     let mut contents = std::fs::read_to_string(file)
         .expect("Couldn't read the file. Read the message part to the right");
     remove_whitespace(&mut contents);
     for (x,y) in contents.chars().enumerate() {
+        //println!("{}{}{}",y,',',y == ',');
         if x > skip.try_into().unwrap() {
+        if y == '>' {index+=1;}
+        if y == '<' {index-=1;}
+        if y == '+' {data[index]+=1;}
+        if y == '-' {data[index]-=1;}
+        if y == ',' {data[index] = string_to_decimals(&args[argindex]).unwrap()[0] as i8;argindex+=1;}
+        if y == '.' {if data[index] != 0{save_me[0] = data[index] as u8;print!("{}",format!("{:?}",decimals_to_string(&save_me)).substring(4,5));}}
         if y == '[' {
             while data[index] > 0 {
             for (x,y) in contents.substring(x,contents.find(&"]".to_string()).expect("Oh")).chars().enumerate() {
@@ -33,7 +39,7 @@ fn main() -> () {
                         if y == '<' {index-=1;}
                         if y == '+' {data[index]+=1;}
                         if y == '-' {data[index]-=1;}
-                        if y == ',' {help_me = args[argindex].clone();data[index] = string_to_decimals(&help_me).unwrap()[0] as i8;argindex+=1;}
+                        if y == ',' {data[index] = string_to_decimals(&args[argindex]).unwrap()[0] as i8;argindex+=1;}
                         if y == '.' {if data[index] != 0{save_me[0] = data[index] as u8;print!("{}",format!("{:?}",decimals_to_string(&save_me)).substring(4,5));}}
                         if index > 30000 {index = 0;}
                         }
@@ -44,19 +50,13 @@ fn main() -> () {
                 if y == '<' {index-=1;}
                 if y == '+' {data[index]+=1;}
                 if y == '-' {data[index]-=1;}
-                if y == ',' {help_me = args[argindex].clone();data[index] = string_to_decimals(&help_me).unwrap()[0] as i8;argindex+=1;}
+                if y == ',' {data[index] = string_to_decimals(&args[argindex]).unwrap()[0] as i8;argindex+=1;}
                 if y == '.' {if data[index] != 0{save_me[0] = data[index] as u8;print!("{}",format!("{:?}",decimals_to_string(&save_me)).substring(4,5));}}
                 if index > 30000 {index = 0;}
                 }
             }
         }
         }
-        if y == '>' {index+=1;}
-        if y == '<' {index-=1;}
-        if y == '+' {data[index]+=1;}
-        if y == '-' {data[index]-=1;}
-        if y == ',' {help_me = args[argindex].clone();data[index] = string_to_decimals(&help_me).unwrap()[0] as i8;argindex+=1;}
-        if y == '.' {if data[index] != 0{save_me[0] = data[index] as u8;print!("{}",format!("{:?}",decimals_to_string(&save_me)).substring(4,5));}}
         if index > 30000 {index = 0;}
         }
     }
